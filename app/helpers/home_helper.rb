d3142fa244a3497a6a293091559f23ad659a5433
alizain721 <alizain721@gmail.com>
2019-01-11 20:02:38 +0500

    Set Hot new arrival items

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -10,4 +10,16 @@ module HomeHelper
   def get_car_lights
     ProductType.where(name:"Car Lights").take.products
   end
+
+  def get_men_shirts
+    ProductType.where(name:"Shirts").take.products
+  end
+
+  def get_men_jeans
+    ProductType.where(name:"Jeans").take.products
+  end
+
+  def get_men_under_garments
+    ProductType.where(name:"Boxers").take.products
+  end
 end
