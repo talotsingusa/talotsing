7769e1f8c23e8ba4e82d23e04853706a3d16b31d
alizain721 <alizain721@gmail.com>
2019-01-11 17:43:19 +0500

    Make slider below all products on homepage working

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,13 @@
 module HomeHelper
+  def get_t_shirts
+    ProductType.where(name:"T-Shirts").take.products
+  end
+
+  def get_blazers
+    ProductType.where(name:"Blazers").take.products
+  end
+
+  def get_car_lights
+    ProductType.where(name:"Car Lights").take.products
+  end
 end
