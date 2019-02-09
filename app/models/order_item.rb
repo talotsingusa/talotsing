6c6a4ae71e460a7354c93ceb896788f67dc066b5
alizain721 <alizain721@gmail.com>
2019-02-09 16:40:22 +0500

    Chnage shopping steps

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,15 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  before_destroy do
+    @order = self.order
+  end
+
+  after_destroy do
+    unless @order.order_items.any?
+      @order.destroy
+    end
+  end
+
 end
