d37a8b8815dda724aa5c2df4b39a21cb9c786dc7
alizain721 <alizain721@gmail.com>
2019-03-21 21:54:36 +0500

    User: Add phone number field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     return root_path if resource.class != String
@@ -43,4 +43,10 @@ class ApplicationController < ActionController::Base
       # do nothing
     end
   end
+
+  protected
+    def configure_permitted_parameters
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:phone_number])
+    end
 end
