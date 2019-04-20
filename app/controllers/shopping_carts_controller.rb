8969b10ab069f0be1df49afa7d5190728864bb06
alizain721 <alizain721@gmail.com>
2019-04-20 07:36:16 +0500

    Payment: Refactor credit card transaction code

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -21,38 +21,12 @@ class ShoppingCartsController < ApplicationController
     else
       redirect_to user_session_path
     end
-    # gon.client_token = generate_client_token
   end
 
   def process_payment
     @order = current_user.orders.last
-    locations_api = SquareConnect::LocationsApi.new
-    begin
-    locations_response = locations_api.list_locations
-      puts locations_response
-    rescue SquareConnect::ApiError => e
-      raise "Error encountered while listing locations: #{e.message}"
-    end
-    location = locations_response.locations.detect do |l|
-      l.capabilities.include?("CREDIT_CARD_PROCESSING")
-    end
-    if location.nil?
-      flash[:notice] = "Your location is not permited to charge a card"
-      redirect_to  checkout_shopping_carts_path and return
-    end
-    transactions_api = SquareConnect::TransactionsApi.new
-    request_body = {
-    :card_nonce => params[:nonce],
-    :amount_money => {
-      :amount => @order.total_price.round(0)*100,
-      :currency => 'USD'},
-      :idempotency_key => SecureRandom.uuid }
-    begin
-      resp = transactions_api.charge(ENV['SQUARE_CONNECT_LOCATION_ID'], request_body)
-    rescue SquareConnect::ApiError => e
-      flash[:notice] = "Error encountered while charging card: #{e.message}"
-      redirect_to checkout_shopping_carts_path and return
-    end
+    location = SquareConnectServices.location
+    SquareConnectServices.transaction(params[:nonce], @order.total_price)
     flash[:notice] = "Card Charged successfully"
     session.delete(:shop_cart)
     @order.update(status:"paid")
--- app/services/square_connect_services.rb
+++ app/services/square_connect_services.rb
@@ -1,5 +1,33 @@
 class SquareConnectServices
   class << self
-    
-  end
+    def location
+      locations_api = SquareConnect::LocationsApi.new
+      locations_response = locations_api.list_locations rescue nil
+      unless locations_response.nil?
+        location = locations_response.locations.detect do |l|
+          l.capabilities.include?("CREDIT_CARD_PROCESSING")
+        end
+        return location
+      end
+      flash[:notice] = "Your location is not permited to charge a card"
+      redirect_to  checkout_shopping_carts_path and return
+    end
+
+    def transaction(nonce, amount)
+      transactions_api = SquareConnect::TransactionsApi.new
+      request_body = {
+        card_nonce: nonce,
+        amount_money: {
+        amount: amount,
+        currency: 'USD'
+      },
+        idempotency_key: SecureRandom.uuid
+      }
+      begin
+        resp = transactions_api.charge(ENV['SQUARE_CONNECT_LOCATION_ID'], request_body)
+      rescue SquareConnect::ApiError => e
+        flash[:notice] = "Error encountered while charging card: #{e.message}"
+        redirect_to checkout_shopping_carts_path and return
+      end
+    end
 end
