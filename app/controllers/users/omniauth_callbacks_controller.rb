8013d4cadb9bc08ef31a2d519364682e3b93209e
alizain721 <alizain721@gmail.com>
2019-05-04 13:06:22 +0500

    Omniauth: Refactor code

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,18 +1,11 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    puts "**********************************************************"
-    puts @user
-    puts "**********************************************************"
-
     if @user.persisted?
-      puts "************************In the if of user persisted **********************************"
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
-      puts "In else************************************************************"
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      puts "In redirect_to user url*********************************************"
       redirect_to new_user_registration_url
     end
   end
@@ -21,7 +14,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
    if @user.persisted?
     sign_in_and_redirect @user
-    set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+    set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
   else
     flash[:error] = 'There was a problem signing you in through Google. Please register or try signing in later.'
     redirect_to new_user_registration_url
