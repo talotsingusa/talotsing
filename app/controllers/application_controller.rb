5f49a090202dd89c52157a358caa2f27a0974b94
alizain721 <alizain721@gmail.com>
2019-06-12 09:52:58 +0500

    Fix redirect issue of customer sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    return root_path if resource.class != String
     user = User.find(resource.id)
-
     if resource.email  == "admin@gmail.com"
       dashboards_path
     elsif resource.profile_type == "vendor"
