d4b358ad2705e6b829208b74c7f12e45c5205cf3
ali zain <alizain721@gmail.com>
2019-07-14 19:40:17 +0500

    Google auth set user first and last name from the responce retuned back

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,8 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.first_name = auth.info.name   # assuming the user model has a name
+      user.first_name = auth.info.first_name   # assuming the user model has a name
+      user.last_name = auth.info.last_name
       user.image = auth.info.image # assuming the user model has an image
     end
   end
