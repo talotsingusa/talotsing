7f8ad95fa8c4d9c4e62d1ec2a68acdc299429304
ali zain <alizain721@gmail.com>
2019-07-14 19:43:32 +0500

    Chnage chat ui for user Set default image for admin Set default image for user

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -1,2 +1,6 @@
 module ConversationsHelper
+  def user_chat_image message
+    return 'admin.jpg' if message.user.admin?
+    message.user.image.present? ? message.user.image : 'user.png'
+  end
 end
