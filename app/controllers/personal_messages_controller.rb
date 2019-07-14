bf30461c9d689c020fde1faf6b7337326c8c1d7a
ali zain <alizain721@gmail.com>
2019-07-14 19:22:43 +0500

    Permit image attribute in strong parameters of personal message

--- app/controllers/personal_messages_controller.rb
+++ app/controllers/personal_messages_controller.rb
@@ -21,7 +21,7 @@ class PersonalMessagesController < ApplicationController
   private
 
   def personal_message_params
-    params.require(:personal_message).permit(:body)
+    params.require(:personal_message).permit(:body, :avatar)
   end
 
   def find_conversation!
